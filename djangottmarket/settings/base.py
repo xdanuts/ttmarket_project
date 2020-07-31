5480354e41c4994cd9a0468cea8a14042febabfb
xdanuts <62208870+xdanuts@users.noreply.github.com>
2020-08-01 00:37:39 +0300

    Deploy

--- djangottmarket/settings/base.py
+++ djangottmarket/settings/base.py
@@ -15,6 +15,9 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(BASE_DIR)
+print('***********')
+print('BASE_DIR', BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
---
+++ djangottmarket/settings/prod.py
@@ -0,0 +1,28 @@
+from djangottmarket.settings.base import *
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+ADMINS = [('Danut', 'danutsxd@gmail.com')]
+
+SERVER_EMAIL = 'danutsxd@gmail.com'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_logs.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
